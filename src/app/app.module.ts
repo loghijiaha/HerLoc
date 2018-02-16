5b24b1d24b75782ad45309af99c3eeef7dba42d4
Loghi <36206296+Loghijiaha@users.noreply.github.com>
2018-02-16 12:24:01 +0530

    hu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,21 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { Geolocation } from '@ionic-native/geolocation';
-
+import {AngularFireModule} from 'angularfire2'
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {RoundProgressModule} from 'angular-svg-round-progressbar';
 import { BookingProvider } from '../providers/booking/booking';
 import {HttpClientModule} from '@angular/common/http';
+var config = {
+  apiKey: "AIzaSyDibwTZvgCLp7iolRF-xJPz6F6KRjOOfHE",
+  authDomain: "herloc-46207.firebaseapp.com",
+  databaseURL: "https://herloc-46207.firebaseio.com",
+  projectId: "herloc-46207",
+  storageBucket: "herloc-46207.appspot.com",
+  messagingSenderId: "50367460104"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -26,7 +35,8 @@ import {HttpClientModule} from '@angular/common/http';
     BrowserModule,
     IonicModule.forRoot(MyApp),
       RoundProgressModule,
-      HttpClientModule
+      HttpClientModule,
+      AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
